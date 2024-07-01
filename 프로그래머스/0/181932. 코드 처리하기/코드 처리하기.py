1710317d8f49d4caddf0a45f365bccc8ba79817e
blackmsk <80528452+blackmsk@users.noreply.github.com>
2024-07-01 10:17:19 +0900

    [level 0] Title: 코드 처리하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,18 @@
+def solution(code):
+    mode = 0
+    ret=""
+    for idx in range(len(code)):
+        if mode==0:
+            if code[idx]!='1':
+                if idx%2==0: ret+=code[idx]
+            else:
+                mode=1
+                continue
+        if mode==1:
+            if code[idx]!='1':
+                if idx%2==1: ret+=code[idx]
+            else:
+                mode=0
+                continue
+    if ret=="": return "EMPTY" 
+    else: return ret
